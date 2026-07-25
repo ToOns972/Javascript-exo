257545de60c7090820c4e2b0a2586cd9b39f1421
Toons972 <alexis.julien.martin@gmail.com>
2026-07-26 00:09:08 +0200

    exo reverse string 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reverse = "";
+  let longueur = string.length;
+  for (let i=0; i<longueur; i++){
+    reverse += string.charAt(longueur-1-i);
+    // la chaine de caractère commence par 0 soit 0=h et 4=O .
+    // la longueur de la chaine est de 5 donc pour commencer à la letre O 
+    // on doit faire 5-1 puis soustraire l'itération i pour obtenir le reste des lettres
+  }
+
+  return reverse;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
