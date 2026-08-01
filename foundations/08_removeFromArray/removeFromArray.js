7c85c02544e3339cf95612bac882d616527e1b6f
Toons972 <alexis.julien.martin@gmail.com>
2026-08-01 12:20:09 +0200

    solution TOP

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -24,6 +24,22 @@ const removeFromArray = function(arr,...args) {
     });
     */
      // bon code pour les tests
+
+     /*
+     // create a new empty array
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
+  */
     let arr2 = [...new Set(arr)]
     args.forEach(args => {
         let key = arr2.indexOf(args);
